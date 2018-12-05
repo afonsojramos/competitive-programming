07cd3295f3d661b638a52f2304f60ad76cc95218
Afonso Ramos <up201506239@fe.up.pt>
2018-12-05 22:40:57 +0000

    Solved part 2

--- src/1.go
+++ src/1.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
-	"utils"
+
+	"github.com/afonsojramos/advent-of-code-2018/src/utils"
 )
 
 func main() {
+	// Part 1
 	dat := utils.ReadLines("1.input")
 	nums := utils.IntList(dat)
 	fmt.Println(utils.IntSum(nums))
+
+	// Part 2
+	sum := 0
+	sums := map[int]int{0: 1}
+	for i := 0; ; i = (i + 1) % len(nums) {
+		sum += nums[i]
+		sums[sum]++
+		if sums[sum] == 2 {
+			fmt.Println(sum)
+			break
+		}
+	}
 }
