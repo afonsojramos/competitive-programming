a56808b6dd8074918afe85d436506a637f1b424d
Afonso Ramos <up201506239@fe.up.pt>
2018-12-06 02:45:44 +0000

    Cleaning house

--- src/day-1/1.go
+++ src/day-1/1.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/afonsojramos/advent-of-code-2018/src/utils"
+	"utils"
 )
 
 func main() {
 	// Part 1
-	dat := utils.ReadLines("1.input")
+	dat := utils.ReadLines("day-1/1.input")
 	nums := utils.IntList(dat)
 	fmt.Println(utils.IntSum(nums))
 
