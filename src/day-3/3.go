9ec5f9c9d65fa26ab0001bba14a78dadf862c47f
Afonso Ramos <up201506239@fe.up.pt>
2018-12-09 00:24:00 +0000

    Solved Day 3

---
+++ src/day-3/3.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"utils"
+)
+
+type claim struct {
+	id   int
+	x, y int
+	w, h int
+}
+
+func (c *claim) apply(grid [][]int) {
+	for x := 0; x < c.w; x++ {
+		for y := 0; y < c.h; y++ {
+			grid[c.x+x][c.y+y]++
+		}
+	}
+}
+
+func (c *claim) overlap(grid [][]int) bool {
+	for x := 0; x < c.w; x++ {
+		for y := 0; y < c.h; y++ {
+			if grid[c.x+x][c.y+y] > 1 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	// Part 1
+	var claims []claim
+	dat := utils.ReadLines("day-3/3.input")
+	for _, line := range dat {
+		var c claim
+		utils.Sscanf(line, "#%d @ %d,%d: %dx%d", &c.id, &c.x, &c.y, &c.w, &c.h)
+		claims = append(claims, c)
+	}
+
+	grid := make([][]int, 1000)
+	for i := range grid {
+		grid[i] = make([]int, 1000)
+	}
+
+	for _, c := range claims {
+		c.apply(grid)
+	}
+
+	n := 0
+	for x := range grid {
+		for _, c := range grid[x] {
+			if c > 1 {
+				n++
+			}
+		}
+	}
+	fmt.Println(n)
+
+	// Part 2
+	for _, c := range claims {
+		if !c.overlap(grid) {
+			fmt.Println(c.id)
+		}
+	}
+}
